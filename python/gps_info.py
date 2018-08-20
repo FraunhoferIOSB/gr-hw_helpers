fa4d5314cee3e7c2438dc3fa28d6fe399cfd671b
Matthias Kollmann <matthias.kollmann@iosb.fraunhofer.de>
2018-08-20 14:58:29 +0200

    stability improvments for UHD devices without gpsdo

--- python/gps_info.py
+++ python/gps_info.py
@@ -42,27 +42,46 @@ class gps_info(gr.sync_block):
             self.udp_port = udp_port
         self.update_rate = update_rate
         self.timer = Timer(self.update_rate, self.timer_tick)
+        
+        
 
 
 
     def start(self):
-        self.uhd_dev = eval("self.parent.%s"%(self.device_name))
 
-        while not self.uhd_dev.get_mboard_sensor('gps_locked').to_bool():
-            pass
+        try:
+            self.uhd_dev = eval("self.parent.%s"%(self.device_name))
+            sensor_names = self.uhd_dev.get_mboard_sensor_names()
+        except:
+            self.uhd_dev = None
+            gr.log.warn("UHD Device not found, re-check the block config")
+            return False
 
-        self.uhd_dev.set_time_source('gpsdo')
-        last = self.uhd_dev.get_time_last_pps()
-        next = self.uhd_dev.get_time_last_pps()
-        while last == next:
-            time.sleep(0.05)
+        
+
+    
+
+        if 'gps_locked' in sensor_names:
+            while not self.uhd_dev.get_mboard_sensor('gps_locked').to_bool():
+                pass
+
+            self.uhd_dev.set_time_source('gpsdo')
+            last = self.uhd_dev.get_time_last_pps()
             next = self.uhd_dev.get_time_last_pps()
-        time.sleep(0.1)
-        
-        self.uhd_dev.set_time_next_pps(uhd.time_spec_t(self.uhd_dev.get_mboard_sensor('gps_time').to_int() + 1))
-        time.sleep(1.1)
-        
-        self.timer.start()
+            while last == next:
+                time.sleep(0.05)
+                next = self.uhd_dev.get_time_last_pps()
+            time.sleep(0.1)
+            
+            self.uhd_dev.set_time_next_pps(uhd.time_spec_t(self.uhd_dev.get_mboard_sensor('gps_time').to_int() + 1))
+            time.sleep(1.1)
+            self.has_gps_sensor = True
+            self.timer.start()
+        else:
+            gr.log.info("No GPSDO found using OS time")
+            self.uhd_dev.set_time_next_pps(uhd.time_spec_t(int(time.time()) + 1))
+            self.has_gps_sensor = False
+            
         return True
 
 
@@ -72,32 +91,33 @@ class gps_info(gr.sync_block):
 
 
 
-    def timer_tick(self):       
+    def timer_tick(self):    
 
-        gps_gpgga = self.uhd_dev.get_mboard_sensor('gps_gpgga').value
-        gps_gprmc = self.uhd_dev.get_mboard_sensor('gps_gprmc').value
-        gps_time =  self.uhd_dev.get_mboard_sensor('gps_time').to_int()
-        gps_locked =  self.uhd_dev.get_mboard_sensor('gps_locked').to_bool()
-        uhd_time_set = False
-        pps_seconds = self.uhd_dev.get_time_last_pps().to_ticks(1.0)
+        if(self.has_gps_sensor):
+            gps_gpgga = self.uhd_dev.get_mboard_sensor('gps_gpgga').value
+            gps_gprmc = self.uhd_dev.get_mboard_sensor('gps_gprmc').value
+            gps_time =  self.uhd_dev.get_mboard_sensor('gps_time').to_int()
+            gps_locked =  self.uhd_dev.get_mboard_sensor('gps_locked').to_bool()
+            uhd_time_set = False
+            pps_seconds = self.uhd_dev.get_time_last_pps().to_ticks(1.0)
 
-        if pps_seconds == gps_time:
-            uhd_time_set = True
+            if pps_seconds == gps_time:
+                uhd_time_set = True
+                
             
-        
-        gps_data = pmt.make_dict()
-        gps_data = pmt.dict_add(gps_data, pmt.intern('gps_time'), pmt.to_pmt(gps_time))
-        gps_data = pmt.dict_add(gps_data, pmt.intern('gps_locked'), pmt.to_pmt(gps_locked))
-        gps_data = pmt.dict_add(gps_data, pmt.intern('usrp_time_is_absolute'), pmt.to_pmt(uhd_time_set))
-        gps_data = pmt.dict_add(gps_data, pmt.intern('usrp_time'), pmt.to_pmt(pps_seconds))
-        self.message_port_pub(pmt.intern('gps_data'), gps_data)
-            
-        if self.udp_socket:
-            # send UDP
-            self.udp_socket.sendto("\r\n" + gps_gprmc +"\r\n" + gps_gpgga, ("127.0.0.1" , self.udp_port))
+            gps_data = pmt.make_dict()
+            gps_data = pmt.dict_add(gps_data, pmt.intern('gps_time'), pmt.to_pmt(gps_time))
+            gps_data = pmt.dict_add(gps_data, pmt.intern('gps_locked'), pmt.to_pmt(gps_locked))
+            gps_data = pmt.dict_add(gps_data, pmt.intern('usrp_time_is_absolute'), pmt.to_pmt(uhd_time_set))
+            gps_data = pmt.dict_add(gps_data, pmt.intern('usrp_time'), pmt.to_pmt(pps_seconds))
+            self.message_port_pub(pmt.intern('gps_data'), gps_data)
+                
+            if self.udp_socket:
+                # send UDP
+                self.udp_socket.sendto("\r\n" + gps_gprmc +"\r\n" + gps_gpgga, ("127.0.0.1" , self.udp_port))
 
-        self.timer = Timer(self.update_rate, self.timer_tick)
-        self.timer.start()
+            self.timer = Timer(self.update_rate, self.timer_tick)
+            self.timer.start()
 
 
 
